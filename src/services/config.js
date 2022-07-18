cbe78661f986b1c8664cd512e6289b5913cd6094
VuDej <drvu47@gmail.com>
2022-07-18 15:17:33 +0200

    build

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-undef */
 const firebaseConfig = {
-  apiKey: AIzaSyAMk9CN2pAIRaX4a1LPlFupBVDEIYE3rS8,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DB_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
